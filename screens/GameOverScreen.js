86280cb67a7f97822f56a8f60f016f95c4a79e18
contrecc <colinccontreary@gmail.com>
2020-03-15 18:49:30 -0700

    Add SafeAreaView

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -15,32 +15,32 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = props => {
   return (
-    <ScrollView>
-      <View style={styles.screen}>
-        <TitleText>The Game is Over!</TitleText>
-        <View style={styles.imageContainer}>
-          <Image
-            fadeDuration={1000}
-            source={require("../assets/success.png")}
-            // source={{
-            //   uri:
-            //     "https://www.telegraph.co.uk/content/dam/Travel/ski/K2-mountain-Andrzej-Bargiel-first-ski-descent-by-Piotr-Pawlus-Red-Bull-Content-Pool-xlarge.jpg"
-            // }}
-            style={styles.image}
-            resizeMode="cover"
-          />
+      <ScrollView>
+        <View style={styles.screen}>
+          <TitleText>The Game is Over!</TitleText>
+          <View style={styles.imageContainer}>
+            <Image
+              fadeDuration={1000}
+              source={require("../assets/success.png")}
+              // source={{
+              //   uri:
+              //     "https://www.telegraph.co.uk/content/dam/Travel/ski/K2-mountain-Andrzej-Bargiel-first-ski-descent-by-Piotr-Pawlus-Red-Bull-Content-Pool-xlarge.jpg"
+              // }}
+              style={styles.image}
+              resizeMode="cover"
+            />
+          </View>
+          <View style={styles.resultContainer}>
+            <BodyText style={styles.resultText}>
+              Your phone needed{" "}
+              <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds
+              to guess the number{" "}
+              <Text style={styles.highlight}>{props.userNumber}</Text>.
+            </BodyText>
+          </View>
+          <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
         </View>
-        <View style={styles.resultContainer}>
-          <BodyText style={styles.resultText}>
-            Your phone needed{" "}
-            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-            guess the number{" "}
-            <Text style={styles.highlight}>{props.userNumber}</Text>.
-          </BodyText>
-        </View>
-        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-      </View>
-    </ScrollView>
+      </ScrollView>
   );
 };
 
